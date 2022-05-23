7f3b97b4ec37b437b5705ef53da88bf03cf0d68a
FarruxTUIT <odamboyevfarrux2244@gmail.com>
2022-05-23 12:59:04 -0700

    aakkk

--- src/main/java/com/example/relation/dto/BookDto.java
+++ src/main/java/com/example/relation/dto/BookDto.java
@@ -1,4 +1,32 @@
 package com.example.relation.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Getter
+@Setter
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class BookDto {
+    private Integer id;
+    @NotBlank(message = "Iltimos muallifni kiriting")
+    @Size(min = 2,max = 50)
+    private String author;
+    @NotBlank(message = "Iltimos sarlavhani kiriting")
+    @Size(min = 2,max = 50)
+    private String title;
+    @NotBlank(message = "Iltimos narxini kiriting")
+    @Size(min = 2,max = 2147483646)
+    private Double price;
+    private String bookImage;
+    private Date publishedDate;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private LocalDateTime deletedAt;
 }

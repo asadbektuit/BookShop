2ffc0644d7aec09ae2bdcd5e8e2e7c5be06bdf4b
FarruxTUIT <odamboyevfarrux2244@gmail.com>
2022-05-23 13:22:13 -0700

    yee boy

--- src/main/java/com/example/relation/controller/BookController.java
+++ src/main/java/com/example/relation/controller/BookController.java
@@ -1,4 +1,40 @@
 package com.example.relation.controller;
 
+
+import com.example.relation.dto.BookDto;
+import com.example.relation.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/books")
 public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> get(@PathVariable ("id") Integer id){
+        BookDto result = bookService.get(id);
+        return ResponseEntity.ok(result);
+    }
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody @Valid BookDto bookDto){
+        BookDto result = bookService.create(bookDto);
+        return ResponseEntity.ok(result);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable ("id") Integer id,@RequestBody BookDto bookDto){
+        BookDto result = bookService.update(id,bookDto);
+        return ResponseEntity.ok(result);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable ("id")Integer id){
+        BookDto result = bookService.delete(id);
+        return ResponseEntity.ok(result);
+    }
+
 }
---
+++ src/main/java/com/example/relation/service/BookService.java
@@ -0,0 +1,24 @@
+package com.example.relation.service;
+
+
+import com.example.relation.dto.BookDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+    public BookDto get(Integer id) {
+        return null;
+    }
+
+    public BookDto create(BookDto bookDto) {
+        return null;
+    }
+
+    public BookDto update(Integer id, BookDto bookDto) {
+        return null;
+    }
+
+    public BookDto delete(Integer id) {
+        return null;
+    }
+}

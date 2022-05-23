e3e0fd77d28f713eef2776ebfd0c140e4c34c810
asadbektuit <davlatovasadbek1@gmail.com>
2022-05-23 23:38:53 +0500

    bewimmm

--- src/main/java/com/example/relation/dto/CustomerDto.java
+++ src/main/java/com/example/relation/dto/CustomerDto.java
@@ -1,29 +1,22 @@
-package com.example.relation.entity;
+package com.example.relation.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Getter
 @Setter
 
-@Entity
-@Table(name = ("customers"))
-public class Customer {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CustomerDto {
     private Integer id;
-
+    @NotBlank
     private String name;
-
     private String surname;
-
     private String password;
-
     private String contact;
-
     private Boolean city;
-
     private Integer email;
 }

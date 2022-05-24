eb783071d09813afc91d046302296f4586f10eb5
FarruxTUIT <odamboyevfarrux2244@gmail.com>
2022-05-23 21:22:37 -0700

    ye boyyfarruxgky

--- src/main/java/com/example/relation/service/BookService.java
+++ src/main/java/com/example/relation/service/BookService.java
@@ -21,27 +21,21 @@ public class BookService {
     public BookDto get(Integer id) {
         Book book = getEntity(id);
         BookDto dto = new BookDto();
-        dto.setAuthor(book.getAuthor());
-        dto.setTitle(book.getTitle());
-        dto.setPrice(book.getPrice());
+        convertEntityToDto(book, dto);
         return dto;
     }
 
     public BookDto create(BookDto bookDto) {
         Book book = new Book();
-        book.setAuthor(bookDto.getAuthor());
-        book.setTitle(bookDto.getTitle());
-        book.setPrice(bookDto.getPrice());
         book.setCreatedAt(LocalDateTime.now());
+        convertDtoToEntity(book,bookDto);
         bookRepository.save(book);
         return bookDto;
     }
 
     public boolean update(Integer id, BookDto bookDto) {
         Book update = getEntity(id);
-        update.setAuthor(bookDto.getAuthor());
-        update.setTitle(bookDto.getTitle());
-        update.setPrice(bookDto.getPrice());
+        convertDtoToEntity(update,bookDto);
         update.setUpdatedAt(LocalDateTime.now());
         bookRepository.save(update);
         return true;
@@ -54,8 +48,20 @@ public class BookService {
         return true;
     }
 
+    public void convertDtoToEntity(Book update,BookDto bookDto) {
+        update.setAuthor(bookDto.getAuthor());
+        update.setTitle(bookDto.getTitle());
+        update.setPrice(bookDto.getPrice());
+    }
+
+    public void convertEntityToDto(Book book, BookDto dto) {
+        dto.setAuthor(book.getAuthor());
+        dto.setTitle(book.getTitle());
+        dto.setPrice(book.getPrice());
+    }
+
     public Book getEntity(Integer id) {
-        Optional<Book> optional =bookRepository.findByIdAndDeletedAtIsNull(id);
+        Optional<Book> optional = bookRepository.findByIdAndDeletedAtIsNull(id);
         if (optional.isEmpty()) {
             throw new BadRequest("Book don't found");
         }
